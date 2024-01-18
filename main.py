02bcef436c8d4413a5d3446ae03a5ec2660eb8e5
anjenn-source <anne.dabuyan@gmail.com>
2024-01-18 08:18:59 +0800

    Add currency API and fix logic

--- main.py
+++ main.py
@@ -1,7 +1,58 @@
 from tkinter import Tk, ttk
 from tkinter import *
 
-from PIL import Image, ImageTk
+
+from PIL import Image
+from PIL import ImageTk
+
+import requests
+from requests.structures import CaseInsensitiveDict
+
+url = "https://api.freecurrencyapi.com/v1/currencies"
+
+headers = CaseInsensitiveDict()
+headers["apikey"] = "fca_live_HVabMXj6fkPsm7MBBKr0z4nduj6JpKVGf71HdHR6"
+
+resp = requests.get(url, headers=headers)
+json_data = resp.json()
+
+
+import freecurrencyapi
+client = freecurrencyapi.Client('fca_live_HVabMXj6fkPsm7MBBKr0z4nduj6JpKVGf71HdHR6')
+api_data = client.latest()
+
+#Functions
+# Function for updating currency values in USD dictionary
+def get_value_in_usd (required_currency):
+    required_currency = str(required_currency).upper()
+    return api_data["data"][required_currency]
+
+# Function for getting currency symbol
+def get_currency_symbol(required_currency):
+    required_currency = str(required_currency).upper()
+    return json_data['data'][f'{required_currency}']['symbol']
+
+
+# Function for converting currency values
+def convert():
+    currency_origin = combo1.get()
+    currency_origin = get_value_in_usd(currency_origin)
+    currency_convert = combo2.get()
+    symbol = get_currency_symbol(currency_convert)
+    currency_convert = get_value_in_usd(currency_convert)
+
+    amount = value.get()
+    amount = float(amount)
+    final_value = (amount / currency_origin) * currency_convert
+    final_value = round(final_value, 2)
+
+    result.config(text=f"{symbol} {final_value}")
+    print(final_value)
+
+
+currency= ['AUD', 'BGN', 'BRL', 'CAD', 'CHF', "CNY", "CZK", "DKK", "EUR", "GBP", "HKD", "HRK", 'HUF', 'IDR', 'ILS', 'INR', 'ISK', 'JPY', 'KRW', 'MXN', 'MYR', 'NOK', 'NZD', 'PHP', 'PLN', 'RON', 'RUB', 'SEK', 'SGD', 'THB', 'TRY','USD','ZAR']
+currency_values = {}
+
 
 #colors
 cor0 = '#FFFFFF' #white
@@ -13,6 +64,8 @@ window.geometry('300x320')
 window.title('Converter')
 window.configure(bg=cor0)
 window.resizable(height = FALSE, width=FALSE)
+
+
 
 #frames
 top = Frame(window, width=300, height=60, bg=cor2)
@@ -30,28 +83,31 @@ app_name = Label(top, image = icon, compound=LEFT, text= 'Currency Converter', h
 app_name.place(x=0, y=0)
 
 #main frame
-result = Label(main, text= ' ', width=16, height=2, pady=7, relief='solid', anchor=CENTER, font=('Ivy 15 bold'), bg=cor0, fg=cor1)
+result = Label(main, text= '', width=16, height=2, pady=7, relief='solid', anchor=CENTER, font=('Ivy 15 bold'), bg=cor0, fg=cor1)
 result.place(x=50, y=10)
-
-currency = ['CAD', 'BRL', 'EUR', 'INR', 'USD']
 
 from_label = Label(main, text= 'From', width=8, height=1, pady=0, padx=0, relief='flat', anchor= NW, font=('Ivy 10 bold'), bg=cor0, fg=cor1)
 from_label.place(x=48, y=90)
 combo1 = ttk.Combobox(main, width=8, justify=CENTER, font=('Ivy 12 bold'))
-combo1['values'] = (currency)
+combo1['values'] = currency
 combo1.place(x=50, y=115)
+
+
 
 
 to_label = Label(main, text= 'To', width=8, height=1, pady=0, padx=0, relief='flat', anchor= NW, font=('Ivy 10 bold'), bg=cor0, fg=cor1)
 to_label.place(x=158, y=90)
 combo2 = ttk.Combobox(main, width=8, justify=CENTER, font=('Ivy 12 bold'))
-combo2['values'] = (currency)
+combo2['values'] = currency
 combo2.place(x=160, y=115)
+
+
 
 value = Entry(main, width=22, justify=CENTER, font=('Ivy 12 bold'), relief=SOLID)
 value.place(x=50, y=155)
+amount = value.get()
 
-button = Button(main, text='Converter', width=19, padx=5, height=1, bg=cor2, fg=cor0, font=('Ivy 12 bold'))
+button = Button(main, text='Convert', width=19, padx=5, height=1, bg=cor2, fg=cor0, font=('Ivy 12 bold'), command=convert)
 button.place(x=50, y=210)
 
 
